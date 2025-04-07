a2cc32c8e0575fafe4aee5d427a65449e7a699fd
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-04-07 12:14:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home.ejs")
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
